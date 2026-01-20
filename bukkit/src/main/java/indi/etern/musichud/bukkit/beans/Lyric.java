101a0f13e7d3a064e63191bb8ab8c205d5c71041
楠ちゃん <kusu@kusu.moe>
2026-01-20 15:20:07 +0800

    Bukkit Plugin Implementation.
    Added a full Bukkit plugin module for MusicHud with Gradle setup, wrapper updates, and project includes. Implemented plugin entrypoint, commands, config, network codecs/protocol, login/music services, API client, data beans, and resources (plugin.yml, config). Also enabled Gradle caching/config cache and proxy settings.

---
+++ bukkit/src/main/java/indi/etern/musichud/bukkit/beans/Lyric.java
@@ -0,0 +1,37 @@
+package indi.etern.musichud.bukkit.beans;
+
+/**
+ * Represents lyric information.
+ */
+public class Lyric {
+    public static final Lyric NONE = new Lyric(-1, "");
+    
+    private int version;
+    private String lyric;
+    
+    public Lyric() {
+        this.version = -1;
+        this.lyric = "";
+    }
+    
+    public Lyric(int version, String lyric) {
+        this.version = version;
+        this.lyric = lyric;
+    }
+    
+    public int getVersion() {
+        return version;
+    }
+    
+    public void setVersion(int version) {
+        this.version = version;
+    }
+    
+    public String getLyric() {
+        return lyric;
+    }
+    
+    public void setLyric(String lyric) {
+        this.lyric = lyric;
+    }
+}
---
+++ bukkit/src/main/java/indi/etern/musichud/bukkit/beans/login/LoginType.java
@@ -0,0 +1,10 @@
+package indi.etern.musichud.bukkit.beans.login;
+
+public enum LoginType {
+    PHONE_PASSWORD,
+    EMAIL_PASSWORD,
+    QR_CODE,
+    DEVICE_CODE,
+    ANONYMOUS,
+    UNLOGGED
+}
